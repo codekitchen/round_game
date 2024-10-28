159d49926f73c87741fc4dacbe8cb0ee3b642be4
Brian Palmer <brian@codekitchen.net>
2024-10-28 09:58:41 -0600

    remove crt effect

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,5 @@
 import { game } from "./game"
-import { engineInit, glInitPostProcess, setCameraPos, setCanvasFixedSize, setEnablePhysicsSolver, setShowWatermark, vec2 } from "./littlejs.esm"
+import { engineInit, setCameraPos, setCanvasFixedSize, setEnablePhysicsSolver, setShowWatermark, vec2 } from "./littlejs.esm"
 import { TetrisGame } from "./tetris_game"
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -12,7 +12,6 @@ function gameInit() {
   // turn off collisions to avoid accidentally using them
   setEnablePhysicsSolver(false);
   setShowWatermark(false);
-  initPostProcess();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -39,60 +38,6 @@ function gameRenderPost() {
   // called after objects are rendered
   // draw effects or hud that appear above all objects
   game.renderPost();
-}
-
-function initPostProcess() {
-  // shader taken from the littlejs breakout example at
-  // https://github.com/KilledByAPixel/LittleJS/blob/a2d86ab196f46c701729b1f2cf9e7a61832beb4d/examples/breakout/game.js
-  const televisionShader = `
-    // Simple TV Shader Code
-    float hash(vec2 p)
-    {
-        p=fract(p*.3197);
-        return fract(1.+sin(51.*p.x+73.*p.y)*13753.3);
-    }
-    float noise(vec2 p)
-    {
-        vec2 i=floor(p),f=fract(p),u=f*f*(3.-2.*f);
-        return mix(mix(hash(i),hash(i+vec2(1,0)),u.x),mix(hash(i+vec2(0,1)),hash(i+1.),u.x),u.y);
-    }
-    void mainImage(out vec4 c, vec2 p)
-    {
-        // put uv in texture pixel space
-        p /= iResolution.xy;
-
-        // apply fuzz as horizontal offset
-        const float fuzz = .0005;
-        const float fuzzScale = 800.;
-        const float fuzzSpeed = 9.;
-        p.x += fuzz*(noise(vec2(p.y*fuzzScale, iTime*fuzzSpeed))*2.-1.);
-
-        // init output color
-        c = texture(iChannel0, p);
-
-        // chromatic aberration
-        const float chromatic = .002;
-        c.r = texture(iChannel0, p - vec2(chromatic,0)).r;
-        c.b = texture(iChannel0, p + vec2(chromatic,0)).b;
-
-        // tv static noise
-        const float staticNoise = .1;
-        c += staticNoise * hash(p + mod(iTime, 1e3));
-
-        // scan lines
-        const float scanlineScale = 1e3;
-        const float scanlineAlpha = .1;
-        c *= 1. + scanlineAlpha*sin(p.y*scanlineScale);
-
-        // black vignette around edges
-        const float vignette = 2.;
-        const float vignettePow = 4.;
-        float dx = 2.*p.x-1., dy = 2.*p.y-1.;
-        c *= 1.-pow((dx*dx + dy*dy)/vignette, vignettePow);
-    }`;
-
-  const includeOverlay = true;
-  glInitPostProcess(televisionShader, includeOverlay);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
